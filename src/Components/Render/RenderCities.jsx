7797d9eaa29ee1088717e21aaee84ab62c1d359d
Temurbek Sabirov <prodota150@gmail.com>
2020-06-04 16:30:23 -0400

    Remove column class

--- src/Components/Render/RenderCities.jsx
+++ src/Components/Render/RenderCities.jsx
@@ -5,41 +5,37 @@ function RenderCities({ zip }) {
   const { cities, isLoading } = useFetchCity(zip);
   return (
     <>
-      <div className="columns">
-        <div className="column">
-          {cities.map((city) => {
-            return (
-              <>
-                {isLoading ? (
-                  <div>LOADING</div>
-                ) : (
-                  <div className="section">
-                    <div className="container">
-                      <div className="card">
-                        <div className="card-header has-centered-text">
-                          {city.City}, {city.State}
-                        </div>
-                        <div className="card-content">
-                          <ul>
-                            <li>State: {city.State}</li>
-                            <li>
-                              Location: ({city.Lat},{city.Long})
-                            </li>
-                            <li>
-                              Population (estimated): {city.EstimatedPopulation}
-                            </li>
-                            <li>Total Wages: {city.TotalWages}</li>
-                          </ul>
-                        </div>
-                      </div>
+      {cities.map((city) => {
+        return (
+          <>
+            {isLoading ? (
+              <div>LOADING</div>
+            ) : (
+              <div className="section">
+                <div className="container">
+                  <div className="card">
+                    <div className="card-header has-centered-text">
+                      {city.City}, {city.State}
+                    </div>
+                    <div className="card-content">
+                      <ul>
+                        <li>State: {city.State}</li>
+                        <li>
+                          Location: ({city.Lat},{city.Long})
+                        </li>
+                        <li>
+                          Population (estimated): {city.EstimatedPopulation}
+                        </li>
+                        <li>Total Wages: {city.TotalWages}</li>
+                      </ul>
                     </div>
                   </div>
-                )}
-              </>
-            );
-          })}
-        </div>
-      </div>
+                </div>
+              </div>
+            )}
+          </>
+        );
+      })}
     </>
   );
 }
