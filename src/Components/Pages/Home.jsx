a080da20175916211c72978e4f596392489064d2
Temurbek Sabirov <prodota150@gmail.com>
2020-06-04 16:49:51 -0400

    Style navigation bar

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -3,22 +3,24 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <div className="container">
-        <div className="title has-text-centered is-size-1">
-          Welcome to ZipCity
-        </div>
-        <div className="box section has-text-centered">
-          <p className="is-size-3">
-            ZipCity is a site where you can search for a zip code of a
-            particular city or search for its zip code. You can click on the
-            Find City button on the navigation bar to start looking for a
-            particular city by entering a valid zip code. You can click on the
-            Find Zip Code button on the navigation bar to start looking for a
-            particular zip code by entering a valid city name
-          </p>
-        </div>
-        <div className="section has-text-centered is-size-3">
-          We hope you consider this app for your future searches
+      <div className="section">
+        <div className="container">
+          <div className="title has-text-centered is-size-1">
+            Welcome to ZipCity
+          </div>
+          <div className="box section has-text-centered">
+            <p className="is-size-3">
+              ZipCity is a site where you can search for a zip code of a
+              particular city or search for its zip code. You can click on the
+              Find City button on the navigation bar to start looking for a
+              particular city by entering a valid zip code. You can click on the
+              Find Zip Code button on the navigation bar to start looking for a
+              particular zip code by entering a valid city name
+            </p>
+          </div>
+          <div className="section has-text-centered is-size-3">
+            We hope you consider this app for your future searches
+          </div>
         </div>
       </div>
     </>
