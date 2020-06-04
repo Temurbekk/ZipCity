bd056e21acfa0ccd28aec2e529a461d30f27801d
Temurbek Sabirov <prodota150@gmail.com>
2020-06-04 15:48:18 -0400

    Implement zip-code search

---
+++ src/Components/Inputs/InputZip.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const InputZip = ({ setSearch, setSearching }) => {
+  const handleChange = (e) => {
+    if (e.target.value === "") {
+      setSearching(true);
+    } else {
+      setSearch(e.target.value);
+      setSearching(false);
+    }
+  };
+  return (
+    <div>
+      <input
+        className="input"
+        type="text"
+        placeholder="Search..."
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default InputZip;
--- src/Components/Pages/FindCity.jsx
+++ src/Components/Pages/FindCity.jsx
@@ -1,64 +1,21 @@
 import React, { useState, useEffect } from "react";
-import useFetchCity from "../../FetchData/useFetchCity";
+import InputZip from "../Inputs/InputZip";
+import RenderCities from "../Render/RenderCities";
 
-const zip = 10016;
 const FindCity = () => {
-  const { cities, isLoading } = useFetchCity(zip);
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState(0);
   const [searching, setSearching] = useState(true);
-  const [filteredCities, setFilteredCities] = useState();
-
-  useEffect(() => {
-    const implementSearch = () => {
-      setFilteredCities(
-        cities.filter((city) => {
-          return city.City.toLowerCase().includes(search.toLowerCase());
-        })
-      );
-    };
-    implementSearch();
-  }, [search, cities]);
 
   return (
     <>
-      {isLoading ? (
-        <h1>Is Loading..</h1>
-      ) : searching ? (
+      {searching ? (
         <div>
-          <input
-            className="input"
-            type="text"
-            placeholder="Search..."
-            onChange={(e) => {
-              setSearch(e.target.value);
-              setSearching(false);
-            }}
-          />
+          <InputZip setSearch={setSearch} setSearching={setSearching} />
         </div>
       ) : (
         <div>
-          <input
-            className="input"
-            type="text"
-            placeholder="Search..."
-            onChange={(e) => {
-              setSearch(e.target.value);
-              if (e.target.value === "") {
-                setSearching(true);
-              }
-            }}
-          />
-          {filteredCities.map((city) => {
-            return (
-              <>
-                <div>State: {city.State}</div>
-                <div>City: {city.City}</div>
-                <div>
-                  Location: ({city.Lat},{city.Long})
-                </div>
-              </>
-            );
-          })}
+          <InputZip setSearch={setSearch} setSearching={setSearching} />
+          <RenderCities zip={search} />
         </div>
       )}
     </>
