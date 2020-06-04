1670c5c32e1b866dfad1a243075fc70384d025f9
Temurbek Sabirov <prodota150@gmail.com>
2020-06-04 16:26:43 -0400

    Basic Styling

--- src/Components/Render/RenderCities.jsx
+++ src/Components/Render/RenderCities.jsx
@@ -4,25 +4,41 @@ import useFetchCity from "../../FetchData/useFetchCity";
 function RenderCities({ zip }) {
   const { cities, isLoading } = useFetchCity(zip);
   return (
-    <div>
-      {cities.map((city) => {
-        return (
-          <>
-            {isLoading ? (
-              <div>LOADING</div>
-            ) : (
-              <div className="container">
-                <div>State: {city.State}</div>
-                <div>City: {city.City}</div>
-                <div>
-                  Location: ({city.Lat},{city.Long})
-                </div>
-              </div>
-            )}
-          </>
-        );
-      })}
-    </div>
+    <>
+      <div className="columns">
+        <div className="column">
+          {cities.map((city) => {
+            return (
+              <>
+                {isLoading ? (
+                  <div>LOADING</div>
+                ) : (
+                  <div className="container">
+                    <div className="card">
+                      <div className="card-header has-centered-text">
+                        {city.City},{city.State}
+                      </div>
+                      <div className="card-content">
+                        <ul>
+                          <li>State: {city.State}</li>
+                          <li>
+                            Location: ({city.Lat},{city.Long})
+                          </li>
+                          <li>
+                            Population (estimated): {city.EstimatedPopulation}
+                          </li>
+                          <li>Total Wages: {city.TotalWages}</li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
