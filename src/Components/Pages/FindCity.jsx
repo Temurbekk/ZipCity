06ee335bc6df17c8cf74fc3f9306f05403b012f3
Temurbek Sabirov <prodota150@gmail.com>
2020-06-04 13:41:43 -0400

    To test the axios request

--- src/Components/Pages/FindCity.jsx
+++ src/Components/Pages/FindCity.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ZipSearch from "./ZipSearch";
+import axios from "axios";
 
+const zip = 10016;
 const FindCity = () => {
-  return <div>Enter zip code to find the city</div>;
+  const [result, setResult] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://ctp-zip-api.herokuapp.com/zip/${zip}`)
+      .then((response) => {
+        setResult(response.data);
+      });
+  }, []);
+  const handleChange = (event) => {
+    setResult(event.target.value);
+  };
+  return (
+    <>
+      {result.map((city) => {
+        console.log(city.City);
+      })}
+      <form>
+        <h1>Zip Code Search</h1>
+        <label>Zip Code: </label>
+        <input type="number" placeholder="Try 10016" onChange={handleChange} />
+        <ZipSearch Zip={result} name="Result" />
+      </form>
+    </>
+  );
 };
 
 export default FindCity;
--- src/FetchData/fetchCity.jsx
+++ src/FetchData/fetchCity.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const fetchCity = () => {
-  return <div>This component fetches cities</div>;
+const BASE_URL = "http://ctp-zip-api.herokuapp.com/zip";
+
+const fetchCity = (zipCode) => {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/${zipCode}`);
+    fetch(url)
+      .then((response) =>
+        response.json().then((data) => {
+          setCities(data);
+          setIsLoading(false);
+        })
+      )
+      .catch((error) => console.log(error));
+  }, [zipCode]);
+  return { cities, isLoading };
 };
 
 export default fetchCity;
